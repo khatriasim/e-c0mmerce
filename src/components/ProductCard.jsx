11eee64e15a3a0dc18b1554b574ef01113b7cbb2
khatriasim <khatriasim111@gmail.com>
2025-08-19 18:52:58 +0545

    finished inrement

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,15 +1,31 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+
+
 
 const ProductCard = ({product}) => {
+
+const [quantity, setquantity] = useState(1)
+
+const decreaseitem = () =>{
+  if(quantity > 1)
+  setquantity(quantity - 1)
+
+}
+const increaseitem = () =>{
+  setquantity(quantity + 1) 
+}
   return (
     <div className='productcard'>
         <img className='product-image' src={product.image} alt="product.image" />
 <h3>Name:{product.name}</h3>
 <p>Price: ${product.price}</p>
  <div className="quantity-selector gap-9 flex">
-          <span>1</span>
-          <button>+</button>
+    <button onClick={decreaseitem}>-</button>
+          <span>{quantity}</span>
+          <button  onClick={increaseitem}>+</button>
         </div>
           <Link to={`/product/${product.id}`}>
           <button>View Details</button>
